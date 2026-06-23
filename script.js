f5336d2a74ac9ab15c2dc29b3aae903a686474bd
The Brook <birukasmerom191919@gmail.com>
2026-06-23 08:08:11 +0200

    commit

--- script.js
+++ script.js
@@ -118,6 +118,30 @@ function calculate() {
   }
 }
 
+/* Block invalid keys */
+display.addEventListener('keydown', (event) => {
+
+  const allowedKeys = [
+    '0', '1', '2', '3', '4',
+    '5', '6', '7', '8', '9',
+    '+', '-', '*', '/', '%', '.',
+    'Backspace',
+    'Delete',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowUp',
+    'ArrowDown',
+    'Tab',
+    'Home',
+    'End',
+    'Enter',
+    'Escape'
+  ];
+
+  if (!allowedKeys.includes(event.key)) {
+    event.preventDefault();
+  }
+});
 
 /* Remove pasted invalid characters */
 display.addEventListener('input', () => {
